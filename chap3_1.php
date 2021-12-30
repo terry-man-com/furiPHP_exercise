ce296a53c648d01ccd51b9a91f648d5a2bb728cc
terry-man-com <terry.man.com@icloud.com>
2021-12-30 15:12:29 +0900

    Chapter3課題

---
+++ chap3_1.php
@@ -0,0 +1,7 @@
+<?php
+
+$blood_types = ["A", "B", "O", "AB"];
+
+foreach ($blood_types as $blood_type) {
+    var_dump($blood_type . "型" . " ". "です");
+}
---
+++ chap3_2.php
@@ -0,0 +1,9 @@
+<?php
+
+foreach (range(1, 100) as $int) {
+    if ($int % 3 == 0) {
+        echo "アホ", " ";
+    } else {
+        echo $int, " ";
+    }
+}
